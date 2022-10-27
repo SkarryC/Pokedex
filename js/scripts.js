97ac248e5f3f791786e533fbfde8512d9d1aac2f
Carinn Skarry <skarryc@gmail.com>
2022-10-27 08:09:54 -0400

    Part two, task 1.5
    Add IIFE to javascript and alter forEach to reflect changes correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
   { name: "Golbat", height:5, type: ["poison, flying"] },
   { name: "Sandslash", height:3, type: ["ground"] },
   { name: "Squirtle", height:1, type: ["water"] },
 ];
 
 
-pokemonList.forEach(function(pokemonList) {
-  document.write(pokemonList.name  + " is " + pokemonList.height + " ft. and her type is " + pokemonList.type + ".<br>");
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name:"Pikachu" });
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemonList) { document.write(pokemonList.name  + " is " + pokemonList.height + " ft. and her type is " + pokemonList.type + ".<br>");
 });
-
-
