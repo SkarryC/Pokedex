62a29ac2955c5baab32350eecbb1208c52be0ea0
Carinn Skarry <skarryc@gmail.com>
2022-10-27 15:07:25 -0400

    Task 1.6
    Added css styling
    function addListItem and showDetails as well as event listener in js.

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,51 @@ let pokemonRepository = (function () {
   { name: "Squirtle", height:1, type: ["water"] },
 ];
 
-
   function add (pokemon) {
     pokemonList.push(pokemon);
   }
+
 
   function getAll() {
     return pokemonList;
   }
 
+
+  function addListItem (pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function (showDetails) {
+    console.log(pokemon);
+});
+  }
+
+  function showDetails (pokemon) {
+    console.log(pokemon)
+  }
+  
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+    
   };
 })();
 
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name:"Pikachu" });
+
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function(pokemonList) { document.write(pokemonList.name  + " is " + pokemonList.height + " ft. and her type is " + pokemonList.type + ".<br>");
+
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+   pokemonRepository.addListItem(pokemon);
 });
+
+
