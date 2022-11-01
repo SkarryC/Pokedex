1aac76fa67764395e3e4b4b03903a586ae5e4268
Carinn Skarry <skarryc@gmail.com>
2022-11-01 09:06:42 -0400

    Task 1.8 Modal
    Added modal to pokemon app

--- js/scripts.js
+++ js/scripts.js
@@ -28,10 +28,58 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      //console.log(pokemon);
+      showModal(pokemon);
     });
   }
+
+  function showModal(pokemon) {
+    let modalContainer = document.querySelector("#modal-container");
+    modalContainer.innerHTML = "";
+
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    let title = document.createElement("h1");
+    title.innerText = pokemon.name;
+
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemon.imageUrl;
+
+    let pokemonHeight = document.createElement("p");
+    pokemonHeight.innerText = "This pokemon is " + pokemon.height + " tall.";
+
+    let pokemonTypes = document.createElement("p");
+    pokemonTypes.innerText = "Type " + pokemon.types;
+
+    modal.appendChild(title);
+    modal.appendChild(pokemonImage);
+    modal.appendChild(pokemonHeight);
+    modal.appendChild(pokemonTypes);
+    modalContainer.appendChild(modal);
+
+    modalContainer.addEventListener("click", (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+
+    modalContainer.classList.add("is-visible");
+  }
+
+  function hideModal() {
+    let modalContainer = document.querySelector("#modal-container");
+    modalContainer.classList.remove("is-visible");
+  }
+
+  window.addEventListener("keydown", (e) => {
+    let modalContainer = document.querySelector("#modal-container");
+    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+      hideModal();
+    }
+  });
 
   function loadList() {
     return fetch(apiUrl)
@@ -61,7 +109,7 @@ let pokemonRepository = (function () {
       .then(function (details) {
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = details.types;
+        item.types = details.types.map((type) => type.type.name).join(",");
       })
       .catch(function (e) {
         console.error(e);
